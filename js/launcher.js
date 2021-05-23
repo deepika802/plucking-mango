6b12e37fbad94a85614553b81f158b5de05f094b
deepika802 <78304984+deepika802@users.noreply.github.com>
2021-05-23 02:16:50 -0700

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,42 @@
+class Launcher{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length: 50
+        }
+
+        this.pointB = pointB;
+        this.chain = Constraint.create(options);
+
+
+       
+        World.add(world,this.chain);   
+    }
+
+    attach(body){
+        this.chain.bodyA = body;
+    }
+
+    
+
+    fly(){
+        this.chain.bodyA = null
+    }
+    
+    display(){
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.pointB;
+
+            strokeWeight(3);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+        
+        
+
+       
+         
+    }
+}
